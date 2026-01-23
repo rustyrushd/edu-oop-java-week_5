abb72a8cb2f4911334e4546abc61d6a641ca9933
rgm <git@rushd.xyz>
2026-01-23 17:31:11 +0000

    K&R braces, 2 space indent.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,22 +2,20 @@ package ie.atu;
 
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Scanner scan1 = new Scanner(System.in);
-        int age = 0;
-        while(true) {
-            System.out.println("Enter your age:");
-            String text = scan1.nextLine().trim();
-            try {
-                age = Integer.parseInt(text);
-                System.out.println("Your entered " + age);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please try again. " + e.getMessage());
-            }
-        }
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan1 = new Scanner(System.in);
+    int age;
+    while (true) {
+      System.out.println("Enter your age:");
+      String text = scan1.nextLine().trim();
+      try {
+        age = Integer.parseInt(text);
+        System.out.println("Your entered " + age);
+        break;
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid input. Please try again. " + e.getMessage());
+      }
     }
+  }
 }
